6ab9c83b2095c2b4cb27cc8e23513e6b05554fa8
Tkkg1994 <luca@grifodev.ch>
2017-11-11 23:33:03 +0100

    first adaption for ironcontrol

--- app/build/generated/source/buildConfig/debug/com/wubydax/romcontrol/v2/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/wubydax/romcontrol/v2/BuildConfig.java
@@ -5,9 +5,9 @@ package com.wubydax.romcontrol.v2;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.wubydax.romcontrol.batcontrolfree";
+  public static final String APPLICATION_ID = "com.wubydax.romcontrol.ironcontrolfree";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 3;
-  public static final String VERSION_NAME = "1.0.3";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
 }
--- app/build/generated/source/r/debug/de/hdodenhof/circleimageview/R.java
+++ app/build/generated/source/r/debug/de/hdodenhof/circleimageview/R.java
@@ -8,16 +8,16 @@ package de.hdodenhof.circleimageview;
 
 public final class R {
     public static final class attr {
-        public static final int civ_border_color = 0x7f0100b3;
-        public static final int civ_border_overlay = 0x7f0100b4;
-        public static final int civ_border_width = 0x7f0100b2;
-        public static final int civ_fill_color = 0x7f0100b5;
+        public static final int civ_border_color = 0x7f040054;
+        public static final int civ_border_overlay = 0x7f040055;
+        public static final int civ_border_width = 0x7f040056;
+        public static final int civ_fill_color = 0x7f040057;
     }
     public static final class styleable {
-        public static final int[] CircleImageView = { 0x7f0100b2, 0x7f0100b3, 0x7f0100b4, 0x7f0100b5 };
-        public static final int CircleImageView_civ_border_color = 1;
-        public static final int CircleImageView_civ_border_overlay = 2;
-        public static final int CircleImageView_civ_border_width = 0;
+        public static final int[] CircleImageView = { 0x7f040054, 0x7f040055, 0x7f040056, 0x7f040057 };
+        public static final int CircleImageView_civ_border_color = 0;
+        public static final int CircleImageView_civ_border_overlay = 1;
+        public static final int CircleImageView_civ_border_width = 2;
         public static final int CircleImageView_civ_fill_color = 3;
     }
 }
